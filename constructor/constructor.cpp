80fa70fd2b464d8d6c6011f233f1dc726cbb34d1
Qilqilll <aqilf612@gmail.com>
2024-05-13 11:52:21 +0700

    memasukan int main mhs

--- constructor/constructor.cpp
+++ constructor/constructor.cpp
@@ -14,16 +14,37 @@ public:
 		nim = 0;
 		nama = "";
 	};
-	Mahasiswa(int iNim) {
+	Mahasiswa(int iNim)
+	{
 		nim = iNim;
 	}
-	Mahasiswa(string iNama) {
+	Mahasiswa(string iNama) 
+	{
 		nama = iNama;
 	}
-	Mahasiswa(int iNim, string iNama);
-	void cetak();
+	Mahasiswa(int iNim, string iNama)
+	{
+		nim = iNim;
+		nama = iNama;
+	}
+
+	void cetak() {
+		cout << "Nim = " << nim << endl;
+		cout << "NAma = " << nama << endl;
+		cout << endl;
+	}
 };
-Mahasiswa::Mahasiswa(int iNim)
+
+int main()
 {
-	nim = iNim;
+	Mahasiswa mhs1;
+	Mahasiswa mhs2(20);
+	Mahasiswa mhs3("Aqil");
+	Mahasiswa mhs4(20,"Damar");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
 }
